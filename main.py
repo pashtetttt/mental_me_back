7fd6c717ecc5f8c5ab5b8d39f435f3243617f74e
pashtetttt <m9152264214@gmail.com>
2024-09-26 22:33:28 +0300

    added server

---
+++ main.py
@@ -0,0 +1,71 @@
+import mne
+import plotly.graph_objects as go
+import os
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.post("/uploadfile/")
+async def upload_and_post_file(file: UploadFile = File(...)):
+    file_location = f"temp_{file.filename}"
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+    
+    raw = mne.io.read_raw_bdf(file_location, preload=True)
+    raw.pick_types(eeg=True)
+    raw.crop(tmin=0, tmax=180)  # Выбор временного интервала от 0 до 180 секунд
+
+    # Получение данных для графика
+    data, times = raw[:, :]
+
+    # Создание интерактивного графика
+    fig = go.Figure()
+    for i in range(data.shape[0]):
+        fig.add_trace(go.Scatter(
+            x=times, y=data[i], mode='lines', name=raw.ch_names[i]))
+
+    # Сохранение графика в HTML
+    output_file = 'plot.html'
+    fig.write_html(output_file)
+
+    # Удаление временного файла
+    if os.path.exists(file_location):
+        os.remove(file_location)
+
+    # Возврат HTML файла
+    with open(output_file, 'r') as f:
+        html_content = f.read()
+
+    # Удаление HTML файла после отправки
+    if os.path.exists(output_file):
+        os.remove(output_file)
+
+    return HTMLResponse(content=html_content)
+    
+
+
+# raw = mne.io.read_raw_bdf('C:\\Users\\Паша\\VSProjects\\eeg\\backend\\example.bdf', preload=True)
+
+
+# raw.pick_types(eeg=True)  # Выбор только EEG каналов
+# raw.crop(tmin=0, tmax=180)  # Выбор временного интервала от 0 до 60 секунд
+
+
+# data, times = raw[:, :]
+
+
+# fig = go.Figure()
+# for i in range(data.shape[0]):
+#     fig.add_trace(go.Scatter(
+#         x=times, y=data[i], mode='lines', name=raw.ch_names[i]))
+
+# output_file = 'plot.html'
+# fig.write_html(output_file)
+
+
+# if os.path.exists(output_file):  
+#     os.remove(output_file)  
+#     print(f'Файл {output_file} был удален.')
+# else:
+#     print(f'Файл {output_file} не найден.')
